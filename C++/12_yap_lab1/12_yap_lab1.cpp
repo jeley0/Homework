e2f9b0d5b5a67f3241cf3941fcc9a4fdca0d88e4
jeley0 <91151123+jeley0@users.noreply.github.com>
2022-06-13 08:13:25 +0700

    Update 12_yap_lab1.cpp

--- C++/12_yap_lab1/12_yap_lab1.cpp
+++ C++/12_yap_lab1/12_yap_lab1.cpp
@@ -1,320 +1,317 @@
 #include <iostream>
+#include <cstdlib>
 #include <fstream>
-#include <time.h>
-#include <stdlib.h>
+#include <ctime>
 
-int error_checking(); //Проверка внешнего файла
-
-void fillarr(double* arr, int n); // Основные действия с массивом
-void printarr(double* arr, int n);
-double* createarr(int n);
-void reassign(double*& arr, int& n, int k, double number);
-void del(double*& arr, int& n, int k);
-void printarr(double* arr, int n);
-
-double choose(); // Выбор ввода случайного числа или с клавиатуры
-
-void task1(double*& arr, int& n); // Задания
-void task2(double*& arr, int& n);
-void task3(double*& arr, int& n);
-void task4(double*& arr, int& n);
-void task5(double*& arr, int& n);
-void task6(double*& arr, int& n);
-void task7(double*& arr, int& n);
-void task8(double*& arr, int& n);
-
-using namespace std;
-
-int main()
+void scanarr(double* arr, int n) //Заполнение массива для 1-й части
 {
-    setlocale(LC_ALL, "Russian");
-
-    int n = error_checking(); //Проверка внешнего файла
-    if (!n)
-    {
-        cout << "Данные файла неверны" << endl;
-        return 0;
-    }
-
-    cout << "Начальный массив:" << endl;
-    double* arr = createarr(n); //Создание, наполнение и вывод массива
-    fillarr(arr, n);
-    printarr(arr, n);
-
-    int choice; //Меню выбора
-    do
-    {
-        cout << endl;
-        cout << "Выберите задание:" << endl;
-        cout << "1 - Добавить элемент в конец массива" << endl;
-        cout << "2 - Добавить элемент в начало массивa" << endl;
-        cout << "3 - Добавить элемент на k - ую позицию в массиве" << endl;
-        cout << "4 - Удалить последний элемент массива" << endl;
-        cout << "5 - Удалить первый элемент массивa" << endl;
-        cout << "6 - Удалить k - ый элемент массива" << endl;
-        cout << "7 - Добавить элемент массива после первого встреченного элемента с заданным значением, если такого значения нет, то выдать предупреждающее сообщение" << endl;
-        cout << "8 - Удалить все элементы массива с заданным значением, если такого значения нет, то выдать предупреждающее сообщение" << endl;
-        cout << "0 - Завершение программы" << endl;
-
-        cin >> choice;
-        cout << endl;
-        switch (choice)
-        {
-        case 1:
-            task1(arr, n);
-            break;
-        case 2:
-            task2(arr, n);
-            break;
-        case 3:
-            task3(arr, n);
-            break;
-        case 4:
-            task4(arr, n);
-            break;
-        case 5:
-            task5(arr, n);
-            break;
-        case 6:
-            task6(arr, n);
-            break;
-        case 7:
-            task7(arr, n);
-            break;
-        case 8:
-            task8(arr, n);
-            break;
-        case 0:
-            cout << "Работа завершена";
-            break;
-        default:
-            cout << "Неправильно введен номер задания" << endl;
-            break;
-        }
-
-    } while (choice != 0);
-    delete[] arr;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 21 + (-10);
+	}
 }
 
-void task1(double*& arr, int& n)
+void twinarr(double* arr, double* arr1, int n) //Дублирование массива
 {
-    double number;
-    number = choose();
-    unsigned int k;
-    reassign(arr, n, n+1, number);
-    printarr(arr, n);
-}
-
-void task2(double*& arr, int& n)
-{
-    double number;
-    number = choose();
-    unsigned int k;
-    reassign(arr, n, 1, number);
-    printarr(arr, n);
-}
-
-void task3(double*& arr, int& n)
-{
-    double number;
-    number = choose();
-    unsigned int k;
-    cout << "Введите номер позиции для вставки:";
-    cin >> k;
-    reassign(arr, n, k, number);
-    printarr(arr, n);
-}
-
-void task4(double*& arr, int& n)
-{
-    del(arr, n, n);
-    printarr(arr, n);
-}
-
-void task5(double*& arr, int& n)
-{
-    del(arr, n, 1);
-    printarr(arr, n);
-}
-
-void task6(double*& arr, int& n)
-{
-    unsigned int k;
-    cout << "Введите номер позиции для удаления:";
-    cin >> k;
-    del(arr, n, k);
-    printarr(arr, n);
-}
-
-void task7(double*& arr, int& n)
-{
-    double x, number;
-    cout << "Введите элемент, после котороего вы хотите добавить элемент:"; cin >> x;
-    number = choose();
-
-    int k = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] == x)
-        {
-            k = i + 1;
-            break;
-        }
-    }
-    if (k > 0)
-    {
-        double* buff = new double[n + 1];
-        for (int i = 0; i < k; i++)
-        {
-            buff[i] = arr[i];
-        }
-        n++;
-        buff[k] = number;
-        for (int i = k + 1; i < n; i++)
-        {
-            buff[i] = arr[i - 1];
-        }
-        delete[] arr;
-        arr = buff;
-    }
-    else cout << "Элемент не найден" << endl;
-    printarr(arr, n);
-}
-
-void task8(double*& arr, int& n)
-{
-    double x;
-    cout << "Введите номер для удаления:" << endl;
-    cin >> x;
-    int k = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] == x)
-        {
-            k++;
-        }
-    }
-    if (k > 0)
-    {
-        k = 0;
-        double* buff = new double[n - k];
-        for (int i = 0; i < n; i++)
-        {
-            if (arr[i] == x)
-            {
-                k++;
-            }
-            else buff[i - k] = arr[i];
-        }
-        n -= k;
-        delete[] arr;
-        arr = buff;
-    }
-    else cout << "Элемент не найден" << endl;
-    printarr(arr, n);
-}
-
-double choose()
-{
-    int m = 1;
-    double a = -10, b = 10;
-    double number;
-    cout << "Какое число использовать?" << endl;
-    cout << "1 - Случайное" << endl;
-    cout << "2 - Ввести с экрана" << endl;
-    cin >> m;
-    switch (m)
-    {
-    case 1:
-    {
-        srand((unsigned)time(NULL));
-        number = ((double)rand() / RAND_MAX) + ((rand() % 101) - 50); //генерация вещественных в промежутке (-50, 51)
-        m = 0;
-        break;
-    }
-    case 2:
-    {
-        cout << "Введите число:";
-        cin >> number;
-        m = 0;
-        break;
-    }
-    default:
-    {
-        cout << "Ошибка, попробуйте снова" << endl;
-        break;
-    }
-    }
-    return number;
-}
-
-void fillarr(double* arr, int n)
-{
-    fstream A("array.txt", ios::in);
-    for (int i = 0; i < n; i++)
-    {
-        A >> arr[i];
-    }
-    A.close();
-}
-
-double* createarr(int n)
-{
-    double* arr = new double[n];
-    return arr;
-}
-
-void reassign(double*& arr, int& n, int k, double number)
-{
-    k--;
-    if (k >= 0 && k <= n)
-    {
-        double* buff = new double[n + 1];
-        for (int i = 0; i < k; i++)
-            buff[i] = arr[i];
-        buff[k] = number;
-        n++;
-        for (int i = k + 1; i < n; i++)
-            buff[i] = arr[i - 1];
-        delete[] arr;
-        arr = buff;
-    }
-    else cout << "error";
-}
-
-void del(double*& arr, int& n, int k)
-{
-    k--;
-    if (k >= 0 && k <= n)
-    {
-        double* buff = new double[n - 1];
-        for (int i = 0; i < k; i++)
-            buff[i] = arr[i];
-        n--;
-        for (int i = k; i < n; i++)
-            buff[i] = arr[i + 1];
-        delete[] arr;
-        arr = buff;
-    }
-    else cout << "error";
+	for (int i = 0; i < n; i++)
+	{
+		arr1[i] = arr[i];
+	}
 }
 
 void printarr(double* arr, int n) //Вывод массива
 {
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl << std::endl;
 }
 
-int error_checking()
+int elements() //Подсчёт количества элементов в файле
 {
-    double a;
-    int k = 0;
-    fstream A("array.txt", ios::in);
-    while (A >> a)
-    {
-        k++;
-    }
-    A.close();
-    return k;
+	double a;
+	int n = 0;
+	std::ifstream A("arr.txt");
+	while (A >> a)
+	{
+		n++;
+	}
+	A.close();
+	return n;
+}
+
+void newarr(double* arr, int n) //Заполнение массива для 2-й части
+{
+	std::ifstream A("arr.txt");
+	for (int i = 0; i < n; i++)
+	{
+		A >> arr[i];
+	}
+	A.close();
+}
+
+void push_back(double*& arr, int& n, int k, int val) //Добавление элемента в массив, задан номер
+{
+	k--;
+	if (k >= 0 && k <= n)
+	{
+		double* newa = new double[n + 1];
+		for (int i = 0; i < n; i++)
+			newa[i] = arr[i];
+		newa[k] = val;
+		n++;
+		for (int i = k + 1; i < n; i++)
+			newa[i] = arr[i - 1];
+		delete[] arr;
+		arr = newa;
+	}
+	else std::cout << "Нет такого элемента.";
+}
+
+void pop_back(double*& arr, int& n, int k) //Удаление элемента, задан номер
+{
+	k--;
+	if (k >= 0 && k <= n)
+	{
+		double* newa = new double[n - 1];
+			for (int i = 0; i < k; i++)
+				newa[i] = arr[i];
+			n--;
+			for (int i = k; i < n; i++)
+				newa[i] = arr[i + 1];
+		delete[] arr;
+		arr = newa;
+	}
+	else std::cout << "Нет такого элемента.";
+}
+
+void push_value(double*& arr, int& n, int value, int val) //Добавление элемента, задано значение
+{
+	int k = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] == value)
+		{
+			k = i + 1;
+			break;
+		}
+	}
+	double* newa = new double[n + 1];
+	if (k > 0)
+	{
+		for (int i = 0; i < k; i++)
+		{
+			newa[i] = arr[i];
+		}
+		n++;
+		newa[k] = val;
+		for (int i = k + 1; i < n; i++)
+		{
+			newa[i] = arr[i - 1];
+		}
+		delete[] arr;
+		arr = newa;
+	}
+	else std::cout << "Элемент не найден" << std::endl;
+}
+
+void pop_value(double*& arr, int& n, int value) //Удаление элемента, задано значение
+{
+	int k = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] == value)
+		{
+			k = i + 1;
+			break;
+		}
+	}
+	double* newa = new double[n + 1];
+	if (k > 0)
+	{
+		for (int i = 0; i < k; i++)
+		{
+			newa[i] = arr[i];
+		}
+		n--;
+		for (int i = k; i < n; i++)
+		{
+			newa[i] = arr[i + 1];
+		}
+		delete[] arr;
+		arr = newa;
+	}
+	else std::cout << "Элемент не найден" << std::endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	srand(time(NULL));
+
+	int n;
+	std::cout << "Введите размер массива: ";
+	std::cin >> n;
+	double* arr = new double[n];
+	double* arr1 = new double[n];
+	scanarr(arr, n);
+	std::cout << "Изначальный массив: ";
+	printarr(arr, n);
+
+	twinarr(arr, arr1, n);
+	for (int i = 0; i < n; i++)
+	{
+		if (arr1[i] < 0)
+			arr1[i] *= arr1[i];
+	}
+	std::cout << "1.Замена отрицательных элементов их квадратами: ";
+	printarr(arr1, n);
+
+	twinarr(arr, arr1, n);
+	int p;
+	std::cout << "2.Введите p: ";
+	std::cin >> p;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr1[i] > p)
+			arr1[i] = sqrt(arr1[i]);
+	}
+	std::cout << "2.Замена элементов больше p их корнями: ";
+	printarr(arr1, n);
+
+	twinarr(arr, arr1, n);
+	int amax = arr1[0], amin = arr1[0], imax = 0, imin = 0, s = 0;
+	for (int i = 1; i < n; i++)
+	{
+		if (arr1[i] > amax)
+		{
+			amax = arr1[i];
+			imax = i;
+		}
+		else if (arr1[i] < amin)
+		{
+			amin = arr1[i];
+			imin = i;
+		}
+	}
+	s = arr1[imax];
+	arr1[imax] += arr1[imin];
+	arr1[imin] *= s;
+	std::cout << "3.Замена max на сумму max и min, а min на их произведение: ";
+	printarr(arr1, n);
+
+	twinarr(arr, arr1, n);
+	int j, n1;
+	if (n % 2 == 1) n1 = n - 1; else n1 = n;
+	for (int i = 0; i < n1; i += 2)
+	{
+		j = arr1[i];
+		arr1[i] = arr1[i + 1];
+		arr1[i + 1] = j;
+	}
+	std::cout << "4.Перемена мест чётного и нечётного элемента: ";
+	printarr(arr1, n);
+
+	delete[] arr;
+	delete[] arr1;
+
+	n = elements();
+	arr = new double[n];
+	newarr(arr, n);
+	std::cout << "Массив из файла: ";
+	printarr(arr, n);
+
+	int x = rand() % 21 + (-10);
+	int y;
+	std::cout << "Введите число: ";
+	std::cin >> y;
+	push_back(arr, n, n + 1, x);
+	push_back(arr, n, n + 1, y);
+	std::cout << "1.Добавить случайный и введёный с клавиатуры элементы в конец: ";
+	printarr(arr, n);
+	delete[] arr;
+	n -= 2;
+
+	arr = new double[n];
+	newarr(arr, n);
+	pop_back(arr, n, n);
+	std::cout << "2.Удалить последний элемент: ";
+	printarr(arr, n);
+	delete[] arr;
+	n++;
+
+	x = rand() % 21 + (-10);
+	y = 0;
+	std::cout << "Введите число: ";
+	std::cin >> y;
+	arr = new double[n];
+	newarr(arr, n);
+	push_back(arr, n, 1, y);
+	push_back(arr, n, 1, x);
+	std::cout << "3.Добавить случайный и введёный с клавиатуры элементы в начало: ";
+	printarr(arr, n);
+	delete[] arr;
+	n -= 2;
+
+	arr = new double[n];
+	newarr(arr, n);
+	pop_back(arr, n, 1);
+	std::cout << "4.Удалить первый элемент: ";
+	printarr(arr, n);
+	delete[] arr;
+	n++;
+
+	x = rand() % 21 + (-10);
+	y = 0;
+	std::cout << "Введите число: ";
+	std::cin >> y;
+	arr = new double[n];
+	newarr(arr, n);
+	int num;
+	std::cout << "Введите номер массива, после которого нужно вставить элементы: ";
+	std::cin >> num;
+	push_back(arr, n, num + 1, x);
+	push_back(arr, n, num + 2, y);
+	std::cout << "5.Добавить случайный и введёный с клавиатуры элементы после элемента с заданным номером: ";
+	printarr(arr, n);
+	delete[] arr;
+	n -= 2;
+
+	arr = new double[n];
+	newarr(arr, n);
+	num = 0;
+	std::cout << "Введите номер массива, после которого нужно удалить элемент: ";
+	std::cin >> num;
+	pop_back(arr, n, num + 1);
+	std::cout << "6.Удалить элемент после элемента с заданным номером: ";
+	printarr(arr, n);
+	delete[] arr;
+	n++;
+
+	x = rand() % 21 + (-10);
+	y = 0;
+	std::cout << "Введите число: ";
+	std::cin >> y;
+	arr = new double[n];
+	newarr(arr, n);
+	double val;
+	std::cout << "Введите значение, после которого нужно вставить элементы: ";
+	std::cin >> val;
+	push_value(arr, n, val, y);
+	push_value(arr, n, val, x);
+	std::cout << "7.Добавить случайный и введёный с клавиатуры элементы после элемента с заданным номером: ";
+	printarr(arr, n);
+	delete[] arr;
+	n -= 2;
+
+	arr = new double[n];
+	newarr(arr, n);
+	val = 0;
+	std::cout << "Введите значение, после которого нужно удалить элемент: ";
+	std::cin >> val;
+	pop_value(arr, n, val);
+	std::cout << "8.Удалить элемент после элемента с заданным значением: ";
+	printarr(arr, n);
+	delete[] arr;
 }
